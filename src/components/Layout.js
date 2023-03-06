399cba8334938e0caeeb5144b4c95e1613f0f8d4
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-03-05 23:34:04 -0500

    add gradient background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import Header from './Header'
 
 const Layout = ({ children }) => {
     return (
-		<>
+		<div className="w-screen h-screen bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-400">
 			<Head>
 				<title>Jared Schips</title>
 				<meta
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
             
 			<Header />
 			{children}
-		</>
+		</div>
 	);
 }
  
